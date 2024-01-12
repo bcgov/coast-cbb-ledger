584defceccf9808c46457de8cad9b2289d9117f0
caitlinmalloch <caitlin.malloch@gmail.com>
2024-01-12 14:55:52 -0800

    landing page header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login/Login';
 import LandingPage from './components/LandingPage/LandingPage';
 
@@ -8,10 +8,10 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Switch>
-          <Route path="/login" component={Login} />
-          <Route path="/" component={LandingPage} />
-        </Switch>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
       </div>
     </Router>
   );
