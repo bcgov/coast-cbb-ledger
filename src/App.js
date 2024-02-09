0aa46b939858ffd8e64337d4ae9428da56f15979
caitlinmalloch <caitlin.malloch@gmail.com>
2024-02-09 15:39:55 -0800

    built pages and components

--- src/App.js
+++ src/App.js
@@ -1,33 +1,54 @@
 // App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Login from './components/Login';
-import Navigation from './components/Navigation';
-import Home from './components/HomePage';
-import ControlLedger from './components/ControlLedger';
-import DeckedCruiseBilling from './components/DeckedCruiseBilling';
-import PipelineMarks from './components/PipelineMarks';
-import MlcOilAndGas from '././components/MlcOilAndGas';
-import TimberMarkReportLookup from './components/TimberMarkReportLookup';
 
-const App = () => {
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import ControlLedger from './pages/ControlLedger';
+import DeckedCruiseBilling from './pages/DeckedCruiseBilling';
+import MlcOilandGas from './pages/MlcOilandGas';
+import PipelineMarks from './pages/PipelineMarks';
+import TimberMarkReportLookupPage from './pages/TimberMarkReportLookupPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+function App() {
   return (
     <Router>
-      <Header />
-      <Navigation />
-      <Routes>
-        <Route path="/login" element={<Login />} /> {/* Add a route for Login */}
-        <Route path="/" element={<Home />} />
-        <Route path="/control-ledger" element={<ControlLedger />} />
-        <Route path="/decked-cruise-billing" element={<DeckedCruiseBilling />} />
-        <Route path="/pipeline-marks" element={<PipelineMarks />} />
-        <Route path="/mlc-oil-and-gas" element={<MlcOilAndGas />} />
-        <Route path="/timber-mark-report-lookup" element={<TimberMarkReportLookup />} />
-      </Routes>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/control-ledger">Control Ledger</Link>
+            </li>
+            <li>
+              <Link to="/decked-cruise-billing">Decked Cruise Billing</Link>
+            </li>
+            <li>
+              <Link to="/mlc-oil-and-gas">Mlc Oil and Gas</Link>
+            </li>
+            <li>
+              <Link to="/pipeline-marks">Pipeline Marks</Link>
+            </li>
+            <li>
+              <Link to="/timber-mark-report-lookup">Timber Mark Report Lookup</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/control-ledger" component={ControlLedger} />
+          <Route path="/decked-cruise-billing" component={DeckedCruiseBilling} />
+          <Route path="/mlc-oil-and-gas" component={MlcOilandGas} />
+          <Route path="/pipeline-marks" component={PipelineMarks} />
+          <Route path="/timber-mark-report-lookup" component={TimberMarkReportLookupPage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </div>
     </Router>
   );
-};
+}
 
 export default App;
-
