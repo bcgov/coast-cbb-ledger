f778a7dbd226bd789aa9aafc3163cd033cd525f6
caitlinmalloch <caitlin.malloch@gmail.com>
2024-03-13 16:21:53 -0700

    update navbar with page links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,12 +7,12 @@ const Navbar = () => {
     <nav className="navigation-main" id="navbar">
       <div className="container">
         <ul>
-          <li><a href=".">Dashboard</a></li>
-          <li><a href=".">Control Ledger</a></li>
-          <li><a href=".">Decked Cruise Billing</a></li>
-          <li><a href=".">Pipeline Marks</a></li>
-          <li><a href=".">MLC Oil and Gas</a></li>
-          <li><a href=".">Timber Mark Report Lookup</a></li>
+          <li><a href="../pages/DashboardPage.js">Dashboard</a></li>
+          <li><a href="../pages/ConrtolLedgerPage.js">Control Ledger</a></li>
+          <li><a href="../pages/DeckedCruiseBillingPage.js">Decked Cruise Billing</a></li>
+          <li><a href="../pages/PipelineMarksPage.js">Pipeline Marks</a></li>
+          <li><a href="../pages/MLCOilandGasPage.js">MLC Oil and Gas</a></li>
+          <li><a href="../pages/TimberMarkReportLookupPage.js">Timber Mark Report Lookup</a></li>
         </ul>
       </div>
     </nav>
